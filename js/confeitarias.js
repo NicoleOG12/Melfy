a8bb73350a0a92bc9f3bb8e707d894e41a8fd4fb
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-07-30 19:15:35 -0300

    novo carrossel de categorias

--- js/confeitarias.js
+++ js/confeitarias.js
@@ -43,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                     </div>
                 </div>
+                
             `;
             
             const nomeLojaElement = card.querySelector('.marca');
