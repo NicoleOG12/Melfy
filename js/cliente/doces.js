55238307c028d4d5c9d15d58e0ced05bde7bd3ee
stela-sm <stelamontenegro21@gmail.com>
2025-11-14 11:13:51 -0300

    fix: adicionando validação de categoria vazia

--- js/cliente/doces.js
+++ js/cliente/doces.js
@@ -197,24 +197,36 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 
 //lógica para buscar por categoria
-  
-document.querySelectorAll(".categoria-click-event-listener").forEach(cat => {
+  document.querySelectorAll(".categoria-click-event-listener").forEach(cat => {
   cat.addEventListener("click", async () => {
+
     const id = cat.dataset.id; 
-    console.log("id: "+ id)
+    console.log("id:", id);
+
     try {
       const resp = await fetch(`${API_URL}/produtos?categoria=${id}`);
-      var produtos = await resp.json();
-      produtos = produtos.result || [];
-      console.log(produtos)
-      renderizarProdutos(produtos)
-      console.log("Produtos da categoria", id, produtos);
+
+      if (resp.status === 204) {
+        console.log("Nenhum produto encontrado para essa categoria.");
+        renderizarProdutos([]); // envia lista vazia
+        return;
+      }
+    
+      const dataCat = await resp.json();
+
+      const produtosCat = Array.isArray(dataCat.result) ? dataCat.result : [];
+
+      console.log("Produtos da categoria", id, produtosCat);
+
+      renderizarProdutos(produtosCat);
+
     } catch (error) {
       console.error("Erro ao buscar produtos:", error);
     }
 
   });
 });
+
 
 
 });
